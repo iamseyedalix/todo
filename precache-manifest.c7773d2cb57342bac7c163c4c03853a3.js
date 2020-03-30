80b7f4516bef53427136f04afbcc686a899b92fa
Seyed Ali Hosseini <iamseyedalix@gmail.com>
2020-03-30 06:33:02 +0430

    Updates

---
+++ precache-manifest.c7773d2cb57342bac7c163c4c03853a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd582203a2548aeef8fbdf7623b5ad6e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c526dd2e7fec6e11f83d",
+    "url": "/todo/static/css/main.f66ebe61.chunk.css"
+  },
+  {
+    "revision": "4447802fd89874c847a5",
+    "url": "/todo/static/js/2.dee710dc.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/todo/static/js/2.dee710dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c526dd2e7fec6e11f83d",
+    "url": "/todo/static/js/main.c0878d37.chunk.js"
+  },
+  {
+    "revision": "6899a14243a1a96f2a54",
+    "url": "/todo/static/js/runtime-main.92dc29ea.js"
+  }
+]);
